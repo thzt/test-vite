c7ff1edf9d680ffa3af6df338aac144a34a39f1a
thzt <thzt.xx@gmail.com>
2021-04-19 12:16:14 +0800

    feat: add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,29 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import {Row,Col} from 'antd'
+
+import classes from './App.module.less'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className={classes.app}>
+      <Row>
+        <Col span="24">24</Col>
+      </Row>
+      <Row>
+        <Col span="12">12</Col>
+        <Col span="12">12</Col>
+      </Row>
+      <Row>
+        <Col span="8">8</Col>
+        <Col span="8">8</Col>
+        <Col span="8">8</Col>
+      </Row>
+      <Row>
+        <Col span="6">6</Col>
+        <Col span="6">6</Col>
+        <Col span="6">6</Col>
+        <Col span="6">6</Col>
+      </Row>
     </div>
   )
 }
